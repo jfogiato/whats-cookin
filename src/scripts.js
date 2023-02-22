aaad5cf4d4ea2cb8d34b2095d01bbf756a500b1e
Christopher Baum <christopherabaum@gmail.com>
2023-02-21 21:41:32 -0600

    fix: Change recipe alt text to not be 'suspicious' and update header class and interpolated classes on createRecipeCards function

--- src/scripts.js
+++ src/scripts.js
@@ -57,7 +57,7 @@ function createRecipeCards(recipes) {
         if(!recipe.saved) heartClass = "heart-icon hidden";
         recipeSection.innerHTML += `
         <article class="recipe-card pointer" data-parent="${recipe.id}">
-            <img class="recipe-img" src="${recipe.image}" data-parent="${recipe.id}" alt="picture of ${recipe.name}">
+            <img class="recipe-img" src="${recipe.image}" data-parent="${recipe.id}" alt="Picture of ${recipe.name}">
             <img class="${heartClass}" data-parent="${recipe.id}" src="./images/heart.png" alt="This recipe is in my recipes!">
             <h2 class="${titleClass}" data-parent="${recipe.id}">${recipe.name}</h2>
         </article>`;
