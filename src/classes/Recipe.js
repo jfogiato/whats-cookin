4b86aa699648ed95b974dfe4bb0e54bd19f00ceb
Joe Fogiato <joe.fogiato@gmail.com>
2023-02-21 19:10:56 -0500

    Refactor: Recipe class methods to have more descriptive variable names

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -12,21 +12,23 @@ class Recipe {
   matchIngredients(ingredientData) {
     const relevantIngredients = ingredientData.filter(ingredient => {
       const recipeIds = this.ingredients.map(ingredient => ingredient.id);
+
       return recipeIds.includes(ingredient.id);
     });
     
-    const ingredientsWithAllData = this.ingredients.reduce((acc, cv) => {
-      const combiner = relevantIngredients.forEach(el => {
-        if(el.id === cv.id) {
+    const ingredientsWithAllData = this.ingredients.reduce((acc, recipeIngredient) => {
+      const combiner = relevantIngredients.forEach(ingredientItem => {
+        if(ingredientItem.id === recipeIngredient.id) {
           acc.push(
           {
-            id: el.id,
-            name: el.name,
-            estimatedCostInCents: el.estimatedCostInCents,
-            quantity: cv.quantity
+            id: ingredientItem.id,
+            name: ingredientItem.name,
+            estimatedCostInCents: ingredientItem.estimatedCostInCents,
+            quantity: recipeIngredient.quantity
           });
         }
       });
+
       return acc;
     }, []);
 
@@ -34,25 +36,29 @@ class Recipe {
   }
   
   listIngredients(ingredientData) {
-    return this.matchIngredients(ingredientData).map(element => {
-      let amount = element.quantity.amount
+    return this.matchIngredients(ingredientData).map(ingredient => {
+      let amount = ingredient.quantity.amount
       if(amount.toString().length > 5) {
-        amount = element.quantity.amount.toFixed(2)
+        amount = ingredient.quantity.amount.toFixed(2)
       }
-      return `${amount} ${element.quantity.unit} ${element.name}`
+
+      return `${amount} ${ingredient.quantity.unit} ${ingredient.name}`
     });  
   }
 
   listCost(ingredientData) {
-    const totalCost = this.matchIngredients(ingredientData).reduce((acc, cv) => {
-      acc += (cv.estimatedCostInCents * cv.quantity.amount);
+    const totalCost = this.matchIngredients(ingredientData).reduce((acc, ingredient) => {
+      acc += (ingredient.estimatedCostInCents * ingredient.quantity.amount);
+
       return acc;
     }, 0);
+
     return Math.round(totalCost * .01);
   }
 
   getInstructions() {
     const instructions = this.instructions.map(step => `${step.instruction}`);
+    
     return instructions;
   }
 
