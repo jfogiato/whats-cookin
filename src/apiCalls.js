c7ce17e455ba4f2fa85168d7dbc36a4544c60689
Joe Fogiato <joe.fogiato@gmail.com>
2023-02-22 17:26:58 -0500

    Refactor: update .then variable name

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 function fetchData(data) {
     return fetch(`https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${data}`)
-           .then(promise => promise.json())
+           .then(response => response.json())
            .catch(error => console.log(`Could not fetch because: ${error}`));
 };
 
